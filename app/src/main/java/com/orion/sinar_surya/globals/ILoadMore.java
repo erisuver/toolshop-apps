ddab8655708afeb40630702f7600875b4b1921dc
eris <sukwibu@gmail.com>
2025-05-13 14:37:47 +0700

    Revert "init"
    This reverts commit 0ea4a1c25f742ae948b35362fa889218d32d8c84.

--- app/src/main/java/com/orion/sinar_surya/globals/ILoadMore.java
+++
@@ -1,5 +0,0 @@
-package com.orion.sinar_surya.globals;
-
-public interface ILoadMore {
-    void onLoadMore();
-}
--- app/src/main/java/com/orion/sinar_surya/utility/GenericFileProvider.java
+++
@@ -1,6 +0,0 @@
-package com.orion.sinar_surya.utility;
-
-import androidx.core.content.FileProvider;
-
-public class GenericFileProvider extends FileProvider {
-}
